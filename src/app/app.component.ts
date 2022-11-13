06f9ac5e138aaebe1209896eadf2d33f84b52e4b
mykolao <mykolaoleskiv@gmail.com>
2022-11-13 17:45:34 +0200

    use CurencyApiService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ICurrency } from "./app.types";
+import { CurrencyApiService } from "./currency-api.service";
 
 @Component({
   selector: "app-root",
   template: `<h1>App</h1>`,
   styles: []
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public isLoading: boolean = true;
+  public currencyValues: ICurrency[] = [];
+
+  constructor(private currencyApiService: CurrencyApiService) {}
+
+  ngOnInit(): void {
+    this.currencyApiService.getLatestValues().subscribe(({ data }) => {
+      this.currencyValues = Object.values(data);
+      this.isLoading = false;
+    });
+  }
+}
