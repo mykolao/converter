e454e81e4b99485b9371dcb748f32e357bfa5a48
mykolao <mykolaoleskiv@gmail.com>
2022-11-13 18:37:15 +0200

    style: footer

--- src/converter/footer/footer.component.ts
+++ src/converter/footer/footer.component.ts
@@ -3,6 +3,6 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
-  styles: []
+  styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent {}
