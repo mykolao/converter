ce5a2fa3e39a4388ae7093b15c8f730582080074
mykolao <mykolaoleskiv@gmail.com>
2022-11-13 18:11:36 +0200

    feat: footer component

--- src/converter/converter.module.ts
+++ src/converter/converter.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ConverterComponent } from "./converter.component";
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [ConverterComponent, HeaderComponent],
+  declarations: [ConverterComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
   exports: [ConverterComponent]
 })
